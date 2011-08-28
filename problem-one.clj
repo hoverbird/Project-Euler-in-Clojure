1129a521f68f5b49d49c0b2799f4bbb9c238bdc4
Patrick Ewing <patrickhenryewing@gmail.com>
2011-08-27 21:41:23 -0700

    First problem solved

---
+++ problem-one.clj
@@ -0,0 +1,10 @@
+; If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+; Find the sum of all the multiples of 3 or 5 below 1000.
+
+(def a-thousand (range 1000))
+(defn divisible-by-5-or-3 [number]
+  (or 
+    (zero? (rem number 5)) 
+    (zero? (rem number 3))))
+
+(reduce + (filter divisible-by-5-or-3 a-thousand))
