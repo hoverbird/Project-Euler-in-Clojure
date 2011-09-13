37d9c31f04adcf8fb8aed34cd310154c67cff7fd
Patrick Ewing <patrickhenryewing@gmail.com>
2011-09-13 00:34:28 -0700

    Close to solving 3

--- src/euler/problem_three.clj
+++ src/euler/problem_three.clj
@@ -1,15 +1,21 @@
-(ns euler.problem_three
+(ns euler.problem-three
   "The prime factors of 13195 are 5, 7, 13 and 29.
    What is the largest prime factor of the number 600851475143?")
 
 (def huge 600851475143)
-
-(defn is-factor [num factor]
-  (zero? (rem num factor)))
-
-; (defn is-prime [num]
-;   (let [factors []]
-;     loop ))
   
-(defn factorial [n]
-  (reduce * (range 1 (inc n))))
+(defn is-factor [num factor]
+  (and (> factor 1)
+       (zero? (rem num factor))
+       (not (= num factor))))
+
+(defn is-prime [num]
+  (and
+    (> num 2)
+    (< (int (Math/sqrt num)) 2)))
+
+(defn factors [num]
+  (filter (fn [i] (is-factor num i)) (range num)))
+
+(defn prime-factors [num]
+  (filter is-prime (factors num)))
--- test/euler/t_problem_three.clj
+++ test/euler/t_problem_three.clj
@@ -1,7 +1,26 @@
 (ns euler.t_problem_three
-    (:use euler.problem_three 
-            clojure.test
-            midje.sweet))
+    (:use euler.problem-three midje.sweet))
 
 (fact
-  (is-factor 4 2) => true)
+  (is-factor 4 2) => true
+  (is-factor 9 3) => true
+  (is-factor 9 4) => false
+  (is-factor 8 10) => false
+  (is-factor 8 8) => false)
+
+(facts "about prime numbers"
+  (is-prime 0) => falsey
+  (is-prime 1) => falsey
+  (is-prime 2) => falsey
+  (is-prime 3) => truthy
+  (is-prime 4) => falsey
+  (is-prime 8) => falsey)
+
+(facts "about factors"
+  (factors 12) => (seq [2 3 4 6])
+  (factors 13195) => (contains [5 7 13 29]))
+
+(facts "about prime factors"
+  (is-prime huge) => false
+  (prime-factors 13195) => (seq [5 7 13 29])
+)
